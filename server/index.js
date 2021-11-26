f4552d978cc0a573a00dc7f96390099646e639f9
Gabriel Mariano <gabrielmarianodejesus@gmail.com>
2021-11-26 17:06:47 -0300

    Retirado axios dos imports

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const axios = require('axios');
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
